4832a56188b6a25db89673c89ce4a63d44da9cc7
utimur <akva_ten@mail.ru>
2019-03-20 21:45:25 +0300

    lab 1 upgrade

--- app/src/main/java/com/example/lab_1/MainActivity.java
+++ app/src/main/java/com/example/lab_1/MainActivity.java
@@ -29,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
         llMain.addView(lv);
         for(int i = 0; i < 1000000; i++)
         {
-            arrayList.add(i+1+"");
+            String digit = digit(i);
+            arrayList.add(digit);
         }
        // listAdapter.notifyDataSetChanged();
     }
@@ -38,5 +39,230 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+    public String digit(int num)
+    {
+        String digit = "";
+        String[] arr_10 = {"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять"};
+        String[] arr_20 = {"десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать"};
+        String[] arr_20_90 = {"двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто"};
+        String[] arr_100 =  {"сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот"};
+        String[] arr_1000 = {"одна тысяча","две тысячи","три тысячи","четыре тысячи","пять тысяч","шесть тысяч","семь тысяч","восемь тысяч","девять тысяч"};
+        String[] arr_10000_20000 = {"десять тысяч","одиннадцать тысяч","двенадцать тысяч","тринадцать тысяч","четырнадцать тысяч","пятнадцать тысяч","шестнадцать тысяч","семнадцать тысяч","восемнадцать тысяч","девятнадцать тысяч"};
+        if (num <=9)
+        {
+            digit += arr_10[num];
+        }
+        if(num > 9 && num < 20)
+        {
+            digit += arr_20[num-10];
+        }
+        if(num > 19 && num < 100)
+        {
+            int prav = num%10;
+            String sprav = arr_10[prav]+"";
+            if(prav == 0)
+            {
+                sprav = "";
+            }
+            int a = num/10;
+            int lev = a%10;
+            digit += arr_20_90[lev-2] +" " + sprav;
+        }
+        if(num >= 100 && num < 1000)
+        {
+            int prav1 = num%10;
+            int prav2 = (num/10)%10;
+            int lev = (num/100)%10;
+            String sprav = "";
+            digit = arr_100[lev-1] + " ";
+            if(prav2 == 0)
+            {
+                if(prav1 == 0)
+                {
+                    sprav = "";
+                }
+                sprav = arr_10[prav1];
+                digit += sprav;
+            }
+            if(prav2 == 1)
+            {
+                sprav += arr_20[prav1];
+                digit+=sprav;
+            }
+            if(prav2 > 1)
+            {
+                if(prav1 == 0)
+                {
+                    sprav = "";
+                }
+                else sprav = arr_10[prav1];
+                digit += arr_20_90[prav2-2] +" " + sprav;
+            }
+        }
+            if( num >=1000 && num < 10000 && num% 1000 == 0)
+            {
+                digit += arr_1000[num/1000-1];
+            }
+            else if ( num >1000 && num < 10000)
+            {
+                int prav1 = num%10;
+                int prav2 = (num/10)%10;
+                int lev2 = (num/100)%10;
+                int lev1 = (num/1000)%10;
+                String sprav = "";
+                if(lev2 == 0)
+                {
+                    digit = arr_1000[lev1-1] + " ";
+                }
+                else{
+                    digit = arr_1000[lev1-1] +" "+ arr_100[lev2-1] + " ";
+                }
+                if(prav2 == 0)
+                {
+                    if(prav1 == 0)
+                    {
+                        sprav = "";
+                    }
+                    sprav = arr_10[prav1];
+                    digit += sprav;
+                }
+                if(prav2 == 1)
+                {
+                    sprav += arr_20[prav1];
+                    digit+=sprav;
+                }
+                if(prav2 > 1)
+                {
+                    if(prav1 == 0)
+                    {
+                        sprav = "";
+                    }
+                    else sprav = arr_10[prav1];
+                    digit += arr_20_90[prav2-2] +" " + sprav;
+                }
+
+            }
+            if(num>= 10000 && num < 20000 && num%1000==0)
+            {
+                digit = arr_10000_20000[num/1000-10];
+            }
+            else if(num >= 10000 && num < 20000)
+            {
+                int prav1 = num%10;
+                int prav2 = (num/10)%10;
+                int lev2 = (num/100)%10;
+                int lev1 = (num/1000)%10;
+                int lev = (num/1000)-10;
+                String sprav = "";
+                digit += arr_10000_20000[lev] + " ";
+
+
+                if(prav2 == 0)
+                {
+                    if(prav1 == 0)
+                    {
+                        sprav = "";
+                    }
+                    sprav = arr_10[prav1];
+                    digit += sprav;
+                }
+                if(prav2 == 1)
+                {
+                    sprav += arr_20[prav1];
+                    digit+=sprav;
+                }
+                if(prav2 > 1)
+                {
+                    if(prav1 == 0)
+                    {
+                        sprav = "";
+                    }
+                    else sprav = arr_10[prav1];
+                    digit += arr_20_90[prav2-2] +" " + sprav;
+                }
+
+            }
+            if(num>=20000 && num < 100000 && num%10000==0)
+            {
+                digit = arr_20_90[num/10000-2] + " тысяч";
+            }
+            else if(num>=20000 && num < 100000)
+            {
+                int prav1 = num%10;
+                int prav2 = (num/10)%10;
+                int lev2 = (num/100)%10;
+                int lev1 = (num/1000)%10;
+                int lev = (num/10000)%10;
+                String sprav = "";
+                digit += arr_20_90[lev-2] + " тысяч ";
+                if(prav2 == 0)
+                {
+                    if(prav1 == 0)
+                    {
+                        sprav = "";
+                    }
+                    sprav = arr_10[prav1];
+                    digit += sprav;
+                }
+                if(prav2 == 1)
+                {
+                    sprav += arr_20[prav1];
+                    digit+=sprav;
+                }
+                if(prav2 > 1)
+                {
+                    if(prav1 == 0)
+                    {
+                        sprav = "";
+                    }
+                    else sprav = arr_10[prav1];
+                    digit += arr_20_90[prav2-2] +" " + sprav;
+                }
+
+            }
+            if(num >= 100000 && num < 1000000 && num % 100000==0)
+            {
+                digit += arr_100[num/100000-1] + " тысяч ";
+            } else if(num >= 100000 && num < 1000000)
+            {
+                int prav1 = num%10;
+                int prav2 = (num/10)%10;
+                int lev2 = (num/100)%10;
+                int lev1 = (num/1000)%10;
+                int lev = (num/10000)%10;
+                String sprav = "";
+                digit += arr_100[num/100000-1] + " тысяч ";
+                if(prav2 == 0)
+                {
+                    if(prav1 == 0)
+                    {
+                        sprav = "";
+                    }
+                    sprav = arr_10[prav1];
+                    digit += sprav;
+                }
+                if(prav2 == 1)
+                {
+                    sprav += arr_20[prav1];
+                    digit+=sprav;
+                }
+                if(prav2 > 1)
+                {
+                    if(prav1 == 0)
+                    {
+                        sprav = "";
+                    }
+                    else sprav = arr_10[prav1];
+                    digit += arr_20_90[prav2-2] +" " + sprav;
+                }
+
+            }
+            if(num == 1000000)
+            {
+                digit = "ОДИН МИЛЛИОН!!!!!!!!!!!!!!!!!!";
+            }
+        return digit;
     }
 }
